8af7e4ac22b1250944b1d633581bb878eb161997
system64fumo <system64fumo@protonmail.com>
2024-07-26 03:11:49 +0300

    Remove unnecessary threads
    Sadly you can't really run stuff on another thread and have it be stable, That or i still suck at thread safety.
    
    That being said seems like avoiding creating 10 billion threads (in large directories) is helpful performance wise (Who would have thought!)

--- src/file.cpp
+++ src/file.cpp
@@ -2,7 +2,6 @@
 
 #include <iostream>
 #include <magic.h>
-#include <thread>
 
 file_entry::file_entry(const std::filesystem::directory_entry &entry) {
 	file_name = entry.path().filename().string();
@@ -19,28 +18,24 @@ file_entry::file_entry(const std::filesystem::directory_entry &entry) {
 	label.set_max_width_chars(0);
 
 	// Figure out the correct icon
-	std::thread file_thread([&, entry]() {
-		file_size = entry.is_regular_file() ? entry.file_size() : 0;
-		is_directory=entry.is_directory();
+	file_size = entry.is_regular_file() ? entry.file_size() : 0;
+	is_directory=entry.is_directory();
 
-		if (is_directory) {
-			image.set_from_icon_name("default-folder");
-			return;
-		}
+	if (is_directory) {
+		image.set_from_icon_name("default-folder");
+		return;
+	}
 
-		// TODO: Improve mime type handling
-		magic_t magic = magic_open(MAGIC_MIME_TYPE);
-		magic_load(magic, nullptr);
-		const char* mime_type = magic_file(magic, entry.path().c_str());
-		file_icon = std::string(mime_type);
-		magic_close(magic);
-		std::replace(file_icon.begin(), file_icon.end(), '/', '-');
+	// TODO: Improve mime type handling
+	magic_t magic = magic_open(MAGIC_MIME_TYPE);
+	magic_load(magic, nullptr);
+	const char* mime_type = magic_file(magic, entry.path().c_str());
+	file_icon = std::string(mime_type);
+	magic_close(magic);
+	std::replace(file_icon.begin(), file_icon.end(), '/', '-');
 
-		// TODO: Handle previews
-		// This does not always correspond to a right mimetype
-		// Maybe use a manual pre defined list instead?
-		image.set_from_icon_name(file_icon);
-	});
-
-	file_thread.detach();
+	// TODO: Handle previews
+	// This does not always correspond to a right mimetype
+	// Maybe use a manual pre defined list instead?
+	image.set_from_icon_name(file_icon);
 }
