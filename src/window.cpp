5275a9c8f827f2ab90be0eaff494df004c91ac7d
system64fumo <system64fumo@tuta.io>
2024-08-15 18:37:11 +0300

    Cleanup
    Move menu stuff to it's own file

--- src/window.cpp
+++ src/window.cpp
@@ -19,6 +19,7 @@ frog::frog() {
 
 	navbar_setup();
 	sidebar_setup();
+	context_menu_setup();
 	load_icon_map();
 
 	box_main.append(box_container);
@@ -63,8 +64,6 @@ frog::frog() {
 	flowbox_files.set_row_spacing(30);
 	flowbox_files.set_column_spacing(30);
 	flowbox_files.set_selection_mode(Gtk::SelectionMode::MULTIPLE);
-
-	context_menu_setup();
 
 	//std::filesystem::current_path().string() // TODO: Add option to use this
 	entry_path.set_text(getenv("HOME"));
@@ -147,101 +146,6 @@ void frog::sidebar_setup() {
 	flowbox_places.signal_child_activated().connect(sigc::mem_fun(*this, &frog::on_places_child_activated));
 }
 
-void frog::context_menu_setup() {
-	// TODO: Separate this into file and folder menus.
-	file_menu = Gio::Menu::create();
-	auto section1 = Gio::Menu::create();
-	auto section2 = Gio::Menu::create();
-	auto section3 = Gio::Menu::create();
-
-	file_menu->append_section(section1);
-	file_menu->append_section(section2);
-	file_menu->append_section(section3);
-
-	auto action_group = Gio::SimpleActionGroup::create();
-	insert_action_group("file", action_group);
-
-	section1->append("Open With", "file.open");
-	action_group->add_action("open", [](){
-		std::cout << "Clicked: open" << std::endl;
-	});
-	section1->append("Rename", "file.rename");
-	action_group->add_action("rename", [](){
-		std::cout << "Clicked: rename" << std::endl;
-	});
-	section1->append("Delete", "file.delete");
-	action_group->add_action("delete", [&](){
-		std::cout << "Clicked: delete"  << std::endl;
-		auto children = flowbox_files.get_selected_children();
-		for (const auto& child : children) {
-			auto file = dynamic_cast<file_entry*>(child->get_child());
-			std::cout << file->path << std::endl;
-			std::filesystem::remove(file->path);
-		}
-	});
-
-	section2->append("Cut", "file.cut");
-	action_group->add_action("cut", [&](){
-		std::cout << "Clicked: cut" << std::endl;
-		Glib::RefPtr<Gdk::Clipboard> clipboard = get_clipboard();
-
-		std::string content = "cut";
-		for (const auto &child : flowbox_files.get_selected_children()) {
-			auto file = dynamic_cast<file_entry*>(child->get_child());
-			content += "\nfile://" + file->path;
-		}
-
-		Glib::ustring mime_type = "x-special/gnome-copied-files";
-		auto bytes = Glib::Bytes::create(content.data(), content.size());
-		auto contentprovider = Gdk::ContentProvider::create(mime_type, bytes);
-		clipboard->set_content(contentprovider);
-	});
-	section2->append("Copy", "file.copy");
-	action_group->add_action("copy", [&](){
-		std::cout << "Clicked: copy" << std::endl;
-		Glib::RefPtr<Gdk::Clipboard> clipboard = get_clipboard();
-
-		std::string content = "copy";
-		for (const auto &child : flowbox_files.get_selected_children()) {
-			auto file = dynamic_cast<file_entry*>(child->get_child());
-			content += "\nfile://" + file->path;
-		}
-
-		Glib::ustring mime_type = "x-special/gnome-copied-files";
-		auto bytes = Glib::Bytes::create(content.data(), content.size());
-		auto contentprovider = Gdk::ContentProvider::create(mime_type, bytes);
-		clipboard->set_content(contentprovider);
-	});
-	section2->append("Paste", "file.paste");
-	action_group->add_action("paste", [&](){
-		std::cout << "Clicked: paste" << std::endl;
-		Glib::RefPtr<Gdk::Clipboard> clipboard = get_clipboard();
-		std::vector<Glib::ustring> mime_types = {"x-special/gnome-copied-files"};
-		clipboard->read_async(mime_types, Glib::PRIORITY_DEFAULT, [&, clipboard](Glib::RefPtr<Gio::AsyncResult>& result){
-			Glib::ustring mime;
-			Glib::RefPtr<Gio::InputStream> data = clipboard->read_finish(result, mime);
-
-			// TODO: Figure out the size automatically
-			gsize count = 1024;
-
-			data->read_bytes_async(count, [&, data](Glib::RefPtr<Gio::AsyncResult>& result){
-				auto bytes = data->read_bytes_finish(result);
-				gsize bytes_size = bytes->get_size();
-				auto byte_data = bytes->get_data(bytes_size);
-				std::cout << std::string(reinterpret_cast<const char*>(byte_data), bytes_size) << std::endl;
-			});
-		});
-	});
-
-	section3->append("Properties", "file.properties");
-	action_group->add_action("properties", [](){
-		std::cout << "Clicked: properties" << std::endl;
-	});
-
-
-	popovermenu_context_menu.set_menu_model(file_menu);
-}
-
 void frog::on_entry_done() {
 	populate_files(entry_path.get_buffer()->get_text().raw());
 }
@@ -280,20 +184,6 @@ void frog::on_places_child_activated(Gtk::FlowBoxChild *child) {
 	place *place_entry = dynamic_cast<place*>(child->get_child());
 	next_paths.clear();
 	populate_files(place_entry->file_path);
-}
-
-void frog::on_right_clicked(const int &n_press,
-							const double &x,
-							const double &y,
-							Gtk::FlowBoxChild *flowbox_child) {
-
-	flowbox_files.select_child(*flowbox_child);
-
-	Gdk::Rectangle rect(flowbox_child->get_width() / 2, flowbox_child->get_height() / 2, 0, 0);
-	popovermenu_context_menu.unparent();
-	popovermenu_context_menu.set_parent(*flowbox_child);
-	popovermenu_context_menu.set_pointing_to(rect);
-	popovermenu_context_menu.popup();
 }
 
 void frog::populate_files(const std::string &path) {
