77830dceb4b6034f9367f690693fed311c4fb346
system64fumo <system64fumo@protonmail.com>
2024-09-04 05:03:22 +0300

    Add mounted disks support
    Show mounted disks on the sidebar.

--- src/disk.hpp
+++ src/disk.hpp
@@ -3,15 +3,16 @@
 #include <string>
 #include <cstdint>
 #include <filesystem>
+#include <map>
 
 class disk {
 	public:
 		disk(const std::filesystem::path& path);
 
-	private:
 		std::string device_name;
 		uint64_t size_in_b;
 		std::vector<std::string> partitions;
 };
 
+std::map<std::string, std::string> get_mounts();
 std::vector<disk> get_disks();
