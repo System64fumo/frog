88550df3276a632ce56f8ff622eddcce919ec737
system64fumo <system64fumo@tuta.io>
2024-09-12 19:51:39 +0300

    Add disk usage bars
    I hate this implementation so bad!!

--- src/place.cpp
+++ src/place.cpp
@@ -1,14 +1,26 @@
 #include "place.hpp"
 #include "xdg_dirs.hpp"
 
-place::place(std::string label_str, std::string path_str, std::string custom_icon) {
+place::place(std::string label_str, std::string path_str, std::string custom_icon, double fraction) {
 	file_path = path_str;
 	get_style_context()->add_class("place");
 	append(image);
-	append(label);
 
 	image.set_margin_end(5);
 	label.set_text(label_str);
+	label.set_halign(Gtk::Align::START);
+
+	if (fraction != 0) {
+		Gtk::Box *box = Gtk::make_managed<Gtk::Box>(Gtk::Orientation::VERTICAL);
+		progressbar_size.set_fraction(fraction);
+		progressbar_size.set_margin_top(5);
+		append(*box);
+		box->set_hexpand(true);
+		box->append(label);
+		box->append(progressbar_size);
+	}
+	else
+		append(label);
 
 	if (custom_icon != "") {
 		image.set_from_icon_name(custom_icon);
--- src/place.hpp
+++ src/place.hpp
@@ -2,14 +2,16 @@
 #include <gtkmm/box.h>
 #include <gtkmm/image.h>
 #include <gtkmm/label.h>
+#include <gtkmm/progressbar.h>
 
 class place : public Gtk::Box {
 	public:
-		place(std::string label_str, std::string path_str, std::string custom_icon = "");
+		place(std::string label_str, std::string path_str, std::string custom_icon = "", double fraction = 0);
 
 		std::string file_path;
 
 	private:
 		Gtk::Image image;
 		Gtk::Label label;
+		Gtk::ProgressBar progressbar_size;
 };
