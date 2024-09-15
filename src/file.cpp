01e8359feacdb7bd91ffda2a26170cb37aca30f1
system64fumo <system64fumo@protonmail.com>
2024-09-15 03:46:06 +0300

    Improve drag and drop support
    Moved drag and drop stuff to the file itself since that makes more sense.. duh.
    Added proper icons to dnd, Still does not do anything though.

--- src/file.cpp
+++ src/file.cpp
@@ -2,9 +2,12 @@
 #include "icons.hpp"
 #include "xdg_dirs.hpp"
 
+#include <gtkmm/dragsource.h>
 #include <gtkmm/stack.h>
 #include <gtkmm/label.h>
 #include <gtkmm/text.h>
+#include <giomm/file.h>
+#include <gtkmm/icontheme.h>
 
 file_entry::file_entry(const std::filesystem::directory_entry &entry) {
 	path = entry.path();
@@ -12,7 +15,7 @@ file_entry::file_entry(const std::filesystem::directory_entry &entry) {
 	set_orientation(Gtk::Orientation::VERTICAL);
 
 	append(image);
-	image.set_pixel_size(64);
+	image.set_pixel_size(icon_size);
 	image.set_from_icon_name("content-loading-symbolic");
 
 	append(label);
@@ -41,25 +44,50 @@ file_entry::file_entry(const std::filesystem::directory_entry &entry) {
 	file_size = entry.is_regular_file() ? entry.file_size() : 0;
 	is_directory=entry.is_directory();
 
+	// Set up drag and drop
+	auto source = Gtk::DragSource::create();
+	source->set_actions(Gdk::DragAction::COPY);
+
+	source->signal_prepare().connect([&](const double &x, const double &y) {
+		// This is probably not the right way to do this?
+		// It copies strings which is fine but we need to copy files
+		Glib::Value<Glib::ustring> ustring_value;
+		ustring_value.init(ustring_value.value_type());
+		ustring_value.set(path);
+		return Gdk::ContentProvider::create(ustring_value);
+	}, false);
+	add_controller(source);
+
+	// Figure out what icon to use
 	if (is_directory) {
 		if (xdg_dirs[path] != "")
-			image.set_from_icon_name(xdg_dirs[path]);
+			file_icon = xdg_dirs[path];
 		else
-			image.set_from_icon_name("default-folder");
-		return;
-	}
-
-	size_t last_dot_pos = file_name.rfind('.');
-	if (last_dot_pos != std::string::npos) {
-		std::string extension = file_name.substr(last_dot_pos + 1);
-		std::string extension_icon = icon_from_extension[extension];
-		if (!extension_icon.empty())
-			image.set_from_icon_name(icon_from_extension[extension]);
-		else
-			image.set_from_icon_name("application-blank");
+			file_icon = "default-folder";
+		
 	}
 	else {
-		// TODO: Re add magic file detection for fallback
-		image.set_from_icon_name("application-blank");
+		size_t last_dot_pos = file_name.rfind('.');
+		if (last_dot_pos != std::string::npos) {
+			std::string extension = file_name.substr(last_dot_pos + 1);
+			std::string extension_icon = icon_from_extension[extension];
+			if (!extension_icon.empty())
+				file_icon = icon_from_extension[extension];
+			else
+				file_icon = "application-blank";
+		}
+		else {
+			// TODO: Re add magic file detection for fallback
+			file_icon = "application-blank";
+		}
 	}
+
+	// Load icons
+	auto icon_theme = Gtk::IconTheme::get_for_display(Gdk::Display::get_default());
+	auto icon_info = icon_theme->lookup_icon(file_icon, icon_size);
+	auto file = icon_info->get_file();
+	auto icon = Gdk::Texture::create_from_file(file);
+
+	image.set(icon);
+	source->set_icon(icon, icon_size / 2, icon_size / 2);
 }
