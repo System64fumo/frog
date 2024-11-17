c73ee6a08a64687222307f384a04a3eee752a242
system64fumo <system64fumo@protonmail.com>
2024-11-17 20:11:36 +0200

    Improve file operations
    Paste/Cut operations should work better now i guess.
    Need to prompt the user on what action to take if a conflict is found.

--- src/file_operations.cpp
+++ src/file_operations.cpp
@@ -1,5 +1,6 @@
 #include "window.hpp"
 #include <fstream>
+#include <set>
 
 std::string frog::decode_url(const std::string& encoded) {
 	std::string decoded;
@@ -21,32 +22,121 @@ std::string frog::decode_url(const std::string& encoded) {
 	return decoded;
 }
 
-void frog::file_op_copy(const std::vector<std::filesystem::path>& file_paths) {
-	for (const auto& path : file_paths) {
-		std::printf("Copying: %s to %s\n", path.string().c_str(), current_path.c_str());
+void frog::file_op(const std::vector<std::filesystem::path>& source, std::filesystem::path destination, const char& action) {
+	std::set<std::filesystem::path> conflicts;
 
-		std::string desired_name = current_path.string() +  "/" + path.filename().string();
-		int dot_pos = desired_name.find_last_of('.');
-		std::string name = desired_name;
-		std::string extension;
-		if (dot_pos != -1) {
-			name = desired_name.substr(0, dot_pos);
-			extension = desired_name.substr(dot_pos);
+	// TODO: Run this in another thread
+	// TODO: Also scan for permissions
+	// TODO: Consider adding a progress indicator by getting the total size of-
+	// -the transfer before running the actions.
+
+	// Scan for conflicts
+	for (const auto& src : source) {
+		if (!std::filesystem::exists(src)) {
+			std::fprintf(stderr, "Source path does not exist: %s\n", src.c_str());
+			continue;
 		}
 
-		// TODO: This should only run if the copy is from the same directory to the same directory
-		// Otherwise prompt the user for what action should be done with existing files
-		int suffix = 0;
-		while (std::filesystem::exists(desired_name)) {
-			suffix++;
-			desired_name = name + "-" + std::to_string(suffix) + extension;
+		std::filesystem::path dest_path = destination / src.filename();
+
+		if (std::filesystem::exists(dest_path)) {
+			conflicts.insert(dest_path);
+		}
+	}
+
+	// TODO: Prompt the user for a resolution to the conflict
+	// Print conflicts
+	if (!conflicts.empty()) {
+		std::printf("Conflicts detected:\n");
+		for (const auto& conflict : conflicts) {
+			std::printf(" - %s\n", conflict.c_str());
+			return;
+		}
+	}
+	else {
+		std::printf("No conflicts detected.\n");
+	}
+
+	// Cancel
+	if (action == 'c') {
+		return;
+	}
+
+	if (!std::filesystem::exists(destination)) {
+		std::filesystem::create_directories(destination);
+	}
+
+	// TODO: Delete action should also run here (Share async and permission check code)
+	// Perform actions
+	for (const auto& src : source) {
+		std::filesystem::path dest_path = destination / src.filename();
+
+		// Merge
+		if (action == 'm') {
+			if (std::filesystem::is_directory(src)) {
+				dest_path = destination / src.filename();
+				if (!std::filesystem::exists(dest_path)) {
+					std::filesystem::create_directories(dest_path);
+				}
+				for (const auto& entry : std::filesystem::recursive_directory_iterator(src)) {
+					std::filesystem::path sub_dest_path = destination / std::filesystem::relative(entry.path(), src.parent_path());
+					if (std::filesystem::is_directory(entry.path())) {
+						std::filesystem::create_directories(sub_dest_path);
+					}
+					else {
+						std::filesystem::copy(entry.path(), sub_dest_path, std::filesystem::copy_options::overwrite_existing);
+					}
+				}
+			}
+			else {
+				std::filesystem::copy(src, dest_path, std::filesystem::copy_options::overwrite_existing);
+			}
 		}
 
-		std::ofstream file(desired_name);
-		if (!file) {
-			std::fprintf(stderr, "Failed to create a new file\n");
+		// Duplicate
+		else if (action == 'd') {
+			int iteration = 1;
+			std::filesystem::path new_dest_path = dest_path;
+			while (std::filesystem::exists(new_dest_path)) {
+				new_dest_path = destination / (src.stem().string() + "_" + std::to_string(iteration++) + src.extension().string());
+			}
+
+			if (std::filesystem::is_directory(src)) {
+				std::filesystem::create_directories(new_dest_path);
+				for (const auto& entry : std::filesystem::recursive_directory_iterator(src)) {
+					std::filesystem::path sub_new_dest_path = new_dest_path / std::filesystem::relative(entry.path(), src);
+					if (std::filesystem::is_directory(entry.path())) {
+						std::filesystem::create_directories(sub_new_dest_path);
+					}
+					else {
+						std::filesystem::copy(entry.path(), sub_new_dest_path);
+					}
+				}
+			}
+			else {
+				std::filesystem::copy(src, new_dest_path);
+			}
 		}
 
-		std::filesystem::copy_file(path, desired_name, std::filesystem::copy_options::overwrite_existing);
+		// Move (Copy then delete)
+		else if (action == 't') {
+			if (std::filesystem::is_directory(src)) {
+				std::filesystem::create_directories(dest_path);
+				for (const auto& entry : std::filesystem::recursive_directory_iterator(src)) {
+					std::filesystem::path sub_dest_path = dest_path / std::filesystem::relative(entry.path(), src);
+					if (std::filesystem::is_directory(entry.path())) {
+						std::filesystem::create_directories(sub_dest_path);
+					}
+					else {
+						std::filesystem::copy(entry.path(), sub_dest_path, std::filesystem::copy_options::overwrite_existing);
+					}
+				}
+				std::filesystem::remove_all(src);
+			}
+			else {
+				std::filesystem::copy(src, dest_path, std::filesystem::copy_options::overwrite_existing);
+				std::filesystem::remove(src);
+			}
+		}
 	}
 }
