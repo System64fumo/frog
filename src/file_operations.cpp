fa9ccb2e781905ecb41bda62b6798a18dc39afea
system64fumo <system64fumo@tuta.io>
2024-11-18 16:54:41 +0200

    Add filesize reporting in file ops
    Report what file is being transferred alongside it's size.

--- src/file_operations.cpp
+++ src/file_operations.cpp
@@ -1,4 +1,5 @@
 #include "window.hpp"
+#include "utils.hpp"
 #include <fstream>
 #include <set>
 
@@ -20,6 +21,27 @@ std::string frog::decode_url(const std::string& encoded) {
 		}
 	}
 	return decoded;
+}
+
+uint calculate_size(const std::filesystem::path& path, std::map<std::filesystem::path, uint>& file_sizes) {
+	uint size = 0;
+
+	if (std::filesystem::is_directory(path)) {
+		for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
+			if (std::filesystem::is_regular_file(entry.path())) {
+				uint file_size = std::filesystem::file_size(entry.path());
+				file_sizes[entry.path()] = file_size;
+				size += file_size;
+			}
+		}
+	}
+	else if (std::filesystem::is_regular_file(path)) {
+		uint file_size = std::filesystem::file_size(path);
+		file_sizes[path] = file_size;
+		size += file_size;
+	}
+
+	return size;
 }
 
 void frog::file_op(const std::vector<std::filesystem::path>& source, std::filesystem::path destination, const char& action) {
@@ -66,10 +88,20 @@ void frog::file_op(const std::vector<std::filesystem::path>& source, std::filesy
 		std::filesystem::create_directories(destination);
 	}
 
+
+	// Calculate total size of transfer
+	uint total_size = 0;
+	std::map<std::filesystem::path, uint> file_sizes;
+	for (const auto& src : source) {
+		total_size += calculate_size(src, file_sizes);
+	}
+	std::printf("Total transfer size: %s\n", to_human_readable(total_size).c_str());
+
 	// TODO: Delete action should also run here (Share async and permission check code)
 	// Perform actions
 	for (const auto& src : source) {
 		std::filesystem::path dest_path = destination / src.filename();
+		std::printf("Transfering: %s which is %s\n", src.c_str(), to_human_readable(file_sizes[src]).c_str());
 
 		// Merge
 		if (action == 'm') {
